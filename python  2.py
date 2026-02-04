85d0c19c0d017f7d82874a4e26e3d614e0307818
SwathiG232 <thanvishswathithanvish@gmail.com>
2026-02-04 05:51:40 -0800

    Add files via upload

---
+++ python  2.py
@@ -0,0 +1,38 @@
+#if
+a=int(input("enter amt:"))
+if a==20:
+   print("ok!!")
+#2
+a=int(input("enter age:"))
+if a<=18:
+   print("can vote")
+#if-else
+   a=int(input("enter amt:"))
+if a<20:
+   print("ok!!")
+            
+else:
+    print("ohh!!")
+#+ve or -ve
+    a=int(input("enter amt:"))
+if a>0:
+    print("positive")
+else:
+    print("negative")
+#elif
+    a=int(input("enter amt:"))
+if a>0:
+    print("positive")
+elif a<0:
+    print("ngative")
+else:
+    print("zero")
+
+
+
+
+#aritmetic operatore
+x=10
+y=20
+print(x+y)
+      
