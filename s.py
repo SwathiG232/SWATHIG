d38502a93352defecbe75dd197071b9e78a67037
SwathiG232 <thanvishswathithanvish@gmail.com>
2026-02-04 05:40:00 -0800

    Add files via upload

---
+++ s.py
@@ -0,0 +1,36 @@
+aritmetic
+a=int(input("enter a number:"))
+b=int(input("enter anther number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+average 3 number
+a=int(input("enter no 1:"))
+b=int(input("enter no 2:"))
+c=int(input("enter no 3:"))
+print("average of 3 number is:",(a+b+c)
+start with x=10 apply
+x=10
+x+=5
+x-=3
+x*=2
+x%=2
+x**=3
+print("x:",x)
+2 number arithmetic
+print("enter two number")
+i=int(input("enter a number"))
+j=int(input("enter a number"))
+k=i+j
+j=k-j
+i=k-i
+print("two number :"i,j")
+      
+            
+
+       
+
+
